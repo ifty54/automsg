c20d7f5761aac819f520d7a5dbaf1cad8eac71cc
ifty54 <31790027+ifty54@users.noreply.github.com>
2021-08-12 18:14:15 +0600

    Added WhatsApp Command

--- automsg.py
+++ automsg.py
@@ -6,3 +6,6 @@ while True:
     time.sleep(2)
     pyautogui.typewrite('This is from a bot')
     pyautogui.press('enter')
+    
+import pywhatkit
+pywhatkit.sendwhatmsg('+88018188888', 'Hi from py', 18, 10)
